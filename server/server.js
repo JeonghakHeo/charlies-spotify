ba7f89e35ccc661f1ce76cd5324327388b92b39f
JeonghakHeo <jeonghak4142@gmail.com>
2022-05-13 11:38:48 +1000

    test path fixed

--- server/server.js
+++ server/server.js
@@ -17,12 +17,17 @@ app.use('/api/spotify', spotifyRoutes)
 const __dirname = path.resolve()
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../charlies-player/build')))
+  app.use(
+    express.static(
+      path.join(__dirname, '../charlies-player/charlies-player/build')
+    )
+  )
 
   app.get('*', (req, res) =>
     res.sendFile(
       path.resolve(
         path.join(__dirname, '../'),
+        'charlies-player',
         'charlies-player',
         'build',
         'index.html'
